340c091107344a63cdf1a57df168a56d1397a9da
0x4b1d <ilsecco94@gmail.com>
2024-05-09 14:10:58 +0000

    Revert for https://github.com/moddroid94/pun_sensor/commit/1987711814d71625507eccbc5b2f75c8f5e9a8f8
    Cambiare id orfana gli altri sensori e non sembra esserci un helper per fare un merge dei sensori, per ora torniamo al behavior di dafult che non introduce breaking changes.
    
    hotfix per nome sensore prezzo fascia corrente

--- custom_components/pun_sensor/sensor.py
+++ custom_components/pun_sensor/sensor.py
@@ -91,22 +91,22 @@ class PUNSensorEntity(CoordinatorEntity, SensorEntity, RestoreEntity):
         # possiamo provare a trasferire i dati o a fixare le statistiche a lungo termine?
         # pros: se si aggiungono sensori o si modificano non va cambiato nulla qua
         # cons: gli id dei sensori cambiano se cambia qualcosa a monte
-        self.entity_id = ENTITY_ID_FORMAT.format(f"pun_{self.fascia.value}")
-        # Disabilitato per testare nuova convenzione, da decidere se cambiarlo o mantenere lo stesso
+        # self.entity_id = ENTITY_ID_FORMAT.format(f"pun_{self.fascia.value}")
+
         # ID univoco sensore basato su un nome fisso
-        # match self.fascia:
-        #     case Fascia.MONO:
-        #         self.entity_id = ENTITY_ID_FORMAT.format("pun_mono_orario")
-        #     case Fascia.F1:
-        #         self.entity_id = ENTITY_ID_FORMAT.format("pun_fascia_f1")
-        #     case Fascia.F2:
-        #         self.entity_id = ENTITY_ID_FORMAT.format("pun_fascia_f2")
-        #     case Fascia.F3:
-        #         self.entity_id = ENTITY_ID_FORMAT.format("pun_fascia_f3")
-        #     case Fascia.F23:
-        #         self.entity_id = ENTITY_ID_FORMAT.format("pun_fascia_f23")
-        #     case _:
-        #         self.entity_id = "none"
+        match self.fascia:
+            case Fascia.MONO:
+                self.entity_id = ENTITY_ID_FORMAT.format("pun_mono_orario")
+            case Fascia.F1:
+                self.entity_id = ENTITY_ID_FORMAT.format("pun_fascia_f1")
+            case Fascia.F2:
+                self.entity_id = ENTITY_ID_FORMAT.format("pun_fascia_f2")
+            case Fascia.F3:
+                self.entity_id = ENTITY_ID_FORMAT.format("pun_fascia_f3")
+            case Fascia.F23:
+                self.entity_id = ENTITY_ID_FORMAT.format("pun_fascia_f23")
+            case _:
+                self.entity_id = "none"
         self._attr_unique_id = self.entity_id
         self._attr_has_entity_name = True
 
@@ -284,7 +284,7 @@ class PrezzoFasciaPUNSensorEntity(FasciaPUNSensorEntity, RestoreEntity):
                 self.coordinator.fascia_corrente
             ]
             self._friendly_name = (
-                f"Prezzo fascia corrente (F{self.coordinator.fascia_corrente.value})"
+                f"Prezzo fascia corrente ({self.coordinator.fascia_corrente.value})"
             )
         else:
             self._available = False
